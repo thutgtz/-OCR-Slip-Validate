9ae590a1530d0ee58b43628f4e1b74658af76ab7
Thut Chayasatit <thut.cha@dplusonline.net>
2022-01-18 17:49:27 +0700

    dockerfile

--- index.js
+++ index.js
@@ -25,6 +25,7 @@ app.use((req, res, next) => {
     console.log(`api is callled %o`, req.originalUrl);
     next();
 });
+/*###################### CREATE API ######################*/
 
 createApi(app);
 
