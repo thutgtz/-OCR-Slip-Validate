bcf70b2e48650b1585b548acf49cf8512f57815e
Thut Chayasatit <thut.cha@dplusonline.net>
2022-01-24 01:54:45 +0700

    0.3:5002

--- src/functions/response.js
+++ src/functions/response.js
@@ -1,4 +1,4 @@
-exports.successed = (res, result, message = 'สำเร็จจจจ', status = 200) => res.status(status).send({
+exports.successed = (res, result, message = 'สำเร็จ', status = 200) => res.status(status).send({
     message,
     result
 })
--- test.js
+++ test.js
@@ -16,7 +16,12 @@ describe("Check Test", function () {
                 .get('/check')
                 .send({})
                 .end(function (err, res) {
-                    expect(JSON.stringify(res.body)).to.equal(JSON.stringify({ "message": "สำเร็จจจจ" }));
+                    expect(JSON.stringify(res.body)).to.equal(JSON.stringify({
+                        "message": "สำเร็จ",
+                        "result": {
+                            v: 0.1
+                        }
+                    }));
                     done();
                 });
         });
