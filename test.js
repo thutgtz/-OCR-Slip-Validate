f51d28670d99d53ffde4f613b198d51e46daf713
Thut Chayasatit <thut.cha@dplusonline.net>
2022-01-24 13:14:11 +0700

    test dev

--- test.js
+++ test.js
@@ -19,7 +19,7 @@ describe("Check Test", function () {
                     expect(JSON.stringify(res.body)).to.equal(JSON.stringify({
                         "message": "สำเร็จ",
                         "result": {
-                            v: 0.1
+                            v: 'dev'
                         }
                     }));
                     done();
